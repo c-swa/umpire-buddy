112bb5a10d56c621f5e963aa4f9cfee77bbda085
Christopher Swayne <chris_swa@icloud.com>
2020-01-30 16:01:46 -0600

    Initial Commit
    Commit contains basic framework for the GUI.

---
+++ app/src/main/java/com/example/umpire_buddy/CounterMachine.java
@@ -0,0 +1,19 @@
+package com.example.umpire_buddy;
+
+/*
+CounterMachine will store and count the number of Balls and Strikes.
+Every time the CounterMachine gets an input from the UI, the Counter will increase the proper
+variable by 1, and subsequently return a value.
+ */
+public class CounterMachine {
+    private int ballCount;
+    private int strikeCount;
+
+    public int getBallCount(){
+        return 0;
+    }
+    public int getStrikeCount(){
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/com/example/umpire_buddy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.umpire_buddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
